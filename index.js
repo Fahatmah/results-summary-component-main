49478fbbd1be6d8dcd390301fad5a0069b0e11d0
fahatmah <fahatmahmabang9@gmail.com>
2023-02-21 12:02:10 +0800

    indentation fixed

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ const DATA = './data.json'
 getScoreSummary()
 
 function getScoreSummary() {
-	fetch(DATA)
-		.then((res) => res.json())
-		.then((data) => {
-			createSummary(data)
-		})
+  fetch(DATA)
+    .then((res) => res.json())
+    .then((data) => {
+      createSummary(data)
+    })
 }
 
 function createSummary(data) {
-	while (summaryContainer.firstChild) {
-		summaryContainer.removeChild(summaryContainer.firstChild)
-	}
-	data.forEach((item) => {
-		summaryContainer.insertAdjacentHTML(
-			'beforeend',
-			`<li>
+  while (summaryContainer.firstChild) {
+    summaryContainer.removeChild(summaryContainer.firstChild)
+  }
+  data.forEach((item) => {
+    summaryContainer.insertAdjacentHTML(
+      'beforeend',
+      `<li>
         <div class="category">
-					<img
+          <img
             src=${item.icon}
-						alt=${item.category}-icon
-						class="category-icon"
-					/>
-					<p>${item.category}</p>
-				</div>
-				<div class="score"><span class="bold">${item.score}</span> / 100</div>
-			</li>`
-		)
-	})
+            alt=${item.category}-icon
+            class="category-icon"
+          />
+          <p>${item.category}</p>
+        </div>
+        <div class="score"><span class="bold">${item.score}</span> / 100</div>
+      </li>`
+    )
+  })
 }
